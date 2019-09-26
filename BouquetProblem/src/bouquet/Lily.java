f32ade1edee328b09dde1f532b25b392ea6b56c1
Sumit Trivedi <sumit_trivedi@epam.com>
2019-09-26 19:28:57 +0530

    Bouquet problem submitted

---
+++ BouquetProblem/src/bouquet/Lily.java
@@ -0,0 +1,10 @@
+package bouquet;
+
+public class Lily implements Flowers {
+	public int getCost()
+	{
+		final int perUnitCost=40;
+		return perUnitCost;
+	}
+
+}
---
+++ BouquetProblem/src/bouquet/Rose.java
@@ -0,0 +1,11 @@
+package bouquet;
+
+public class Rose implements Flowers {
+	
+	public int getCost()
+	{
+		final int perUnitCost=50;
+		return perUnitCost;
+	}
+
+}
